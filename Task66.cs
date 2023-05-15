de4a21ac35c0dbac9d46ab21a8ad27a0ba25ee70
AlinaEliseeva <130744735+AlinaEliseeva@users.noreply.github.com>
2023-05-15 21:48:28 +0300

    Add files via upload

---
+++ Task66.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetNumber(string massage)
+{
+    int resultNumber = 0;
+    Console.WriteLine(massage);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод!");
+        }
+    }
+    return resultNumber;
+}
+
+int SumRec(int m, int n)
+{
+    if (m == n) return m;
+    else return m + SumRec(m + 1, n);
+}
+
+int numberM = GetNumber("Введите число m:");
+int numberN = GetNumber("Введите число n:");
+Console.WriteLine($"Сумма натуральных чисел от {numberM} до {numberN} = {SumRec(numberM, numberN)}");
