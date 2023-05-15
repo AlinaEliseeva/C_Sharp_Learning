c2c2a206acc5de66e3e7c0e07576019eca30d6f2
AlinaEliseeva <130744735+AlinaEliseeva@users.noreply.github.com>
2023-05-15 21:47:47 +0300

    Add files via upload

---
+++ Task64.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int GetNumber(string massage)
+{
+    int resultNumber = 0;
+    Console.WriteLine(massage);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод!");
+        }
+    }
+    return resultNumber;
+}
+
+string NumbersRec(int n)
+{
+    if (n > 1) return $"{n} " + NumbersRec(n - 1);
+    else return $"{n} ";
+}
+
+int numberN = GetNumber("Введите число n:");
+Console.WriteLine($"Натуральные чисела от {numberN} до 1 = {NumbersRec(numberN)}");
